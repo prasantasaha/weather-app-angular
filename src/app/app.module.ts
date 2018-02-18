12a3c59e165afa6963b6caf1aa538036a88d00f8
Prasanta Saha <prasanta.work@outlook.com>
2018-02-18 11:11:41 -0800

    fixed deploying to Github Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,4 +22,9 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor() {
+    console.log("weather-app-angular");
+  }
+}
